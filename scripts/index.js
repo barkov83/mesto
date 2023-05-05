acb9bc95adee82840473431041aef18bffdd2024
barkov83 <barkov83@yandex.ru>
2023-05-06 00:34:47 +0400

    валидация

--- scripts/index.js
+++ scripts/index.js
@@ -71,43 +71,110 @@ const popupNewplaceWindow = document.querySelector('.popup_photo');
 const photoInWindow = document.querySelector('.popup__photo-image');
 const namePhotoInWindow = document.querySelector('.popup__photo-caption');
 
-const createCard = (item) => {
-	const placeElement = elementsContainerTemplate.querySelector('.card').cloneNode(true);
-	const cardImage = placeElement.querySelector('.card__image');
-	cardImage.src = item.link;
-	cardImage.alt = item.name;
 
-	placeElement.querySelector('.card__title').textContent = item.name;
+// const createCard = (item) => {
+// 	const placeElement = elementsContainerTemplate.querySelector('.card').cloneNode(true);
+// 	const cardImage = placeElement.querySelector('.card__image');
+// 	cardImage.src = item.link;
+// 	cardImage.alt = item.name;
 
-	const likeButton = placeElement.querySelector('.card__like');
-	likeActive = () => {
-		likeButton.classList.toggle('card__like_active');
+// 	placeElement.querySelector('.card__title').textContent = item.name;
+
+// 	const likeButton = placeElement.querySelector('.card__like');
+// 	likeActive = () => {
+// 		likeButton.classList.toggle('card__like_active');
+// 	}
+// 	likeButton.addEventListener('click', likeActive);
+
+// 	const trashButton = placeElement.querySelector('.card__trash');
+// 	trashActive = () => {
+// 		placeElement.remove();
+// 	}
+// 	trashButton.addEventListener('click', trashActive);
+
+// 	/*открытие окна с большой картинкой*/
+// 	openPopupNewplaceWindow = () => {
+// 		openPopup(popupNewplaceWindow);
+
+// 		photoInWindow.src = item.link;
+// 		photoInWindow.alt = item.name;
+// 		namePhotoInWindow.textContent = item.name;
+// 	}
+
+// 	cardImage.addEventListener('click', openPopupNewplaceWindow);
+
+// 	return placeElement;
+// }
+
+
+/*Создаю класс Кард*/
+class Card {	
+	constructor(data, templateSelector) { 		
+		this._name = data.name;
+		this._link = data.link;
+		this._templateSelector = templateSelector;		
 	}
-	likeButton.addEventListener('click', likeActive);
 
-	const trashButton = placeElement.querySelector('.card__trash');
-	trashActive = () => {
-		placeElement.remove();
-	}
-	trashButton.addEventListener('click', trashActive);
-
-	/*открытие окна с большой картинкой*/
-	openPopupNewplaceWindow = () => {
-		openPopup(popupNewplaceWindow);
-
-		photoInWindow.src = item.link;
-		photoInWindow.alt = item.name;
-		namePhotoInWindow.textContent = item.name;
+	_getTemplate() {		
+		const cardElement = document
+			.querySelector(this._templateSelector) // используем this._templateSelector было:('.elements__list-template')
+			.content
+			.querySelector('.card')
+			.cloneNode(true);
+		
+		return cardElement;
 	}
 
-	cardImage.addEventListener('click', openPopupNewplaceWindow);
+	generateCard() {
+		this._element = this._getTemplate();
+		this._setEventListeners();
+		this._element.querySelector('.card__image').src = this._link;
+		this._element.querySelector('.card__title').textContent = this._name;
+		this._likeIcon = this._element.querySelector('.card__like');
+		this._deleteCard = this._element.querySelector('.card');	
 
-	return placeElement;
+		return this._element;
+	}
+
+	_handleOpenPopup() {
+		photoInWindow.src = this._link;
+		namePhotoInWindow.textContent = this._name;
+		popupNewplaceWindow.classList.add('popup_opened');
+	}
+
+	_handleLikeCard() {
+		this._likeIcon.classList.toggle('card__like_active');
+	}
+
+	_handleDeleteCard() {
+		this._element.remove();
+	}
+
+	// _handleClosePopup() {
+	// 	photoInWindow.src = '';
+	// 	popupNewplaceWindow.classList.remove('popup_opened');
+	// }
+
+	_setEventListeners() {		
+		this._element.querySelector('.card__image').addEventListener('click', () => this._handleOpenPopup());
+		this._element.querySelector('.card__like').addEventListener('click', () => this._handleLikeCard());
+		this._element.querySelector('.card__trash').addEventListener('click', () => this._handleDeleteCard());
+
+	}
 }
 
 initialCards.forEach((item) => {
-	elementsContainer.append(createCard(item));
-})
+	const card = new Card(item, '.elements__list-template');
+	const cardElement = card.generateCard();
+
+	// Добавляем в DOM
+	document.querySelector('.elements__list').append(cardElement);
+});
+
+
+// initialCards.forEach((item) => {
+// 	elementsContainer.append(createCard(item));
+// })
 
 
 const nameNewplaceFormInput = document.querySelector('#popup__imagename');
